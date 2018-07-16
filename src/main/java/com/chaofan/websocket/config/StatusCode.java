d4a869a80ed04d64ad51790cf6463e4e3f30c20e
Chaofan <894195915@qq.com>
2018-07-17 01:49:14 +0800

    初始化

---
+++ src/main/java/com/chaofan/websocket/config/StatusCode.java
@@ -0,0 +1,12 @@
+package com.chaofan.websocket.config;
+
+/**
+ * Created by yuanwai (<a href="mailto:lxdhzzj@gmail.com">lxdhzzj@gmail.com</a>)
+ * Create Time: 2015/7/8 18:06
+ * Description:
+ */
+public interface StatusCode {
+
+    //新用户上线
+    Integer INTO_ROOM = 1;
+}
