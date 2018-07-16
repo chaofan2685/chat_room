3d82abcd8e4e356197b2247c2b8cf82f1da7340f
Chaofan <894195915@qq.com>
2018-07-17 02:08:05 +0800

    修改打包方式为打war包

--- src/main/java/com/chaofan/websocket/config/WebSocketConfig.java
+++ src/main/java/com/chaofan/websocket/config/WebSocketConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
  * Created by Chaofan at 2018/7/4 15:56
  * email:chaofan2685@qq.com
  **/
-@Configuration
+//@Configuration
 public class WebSocketConfig {
 
     @Bean
